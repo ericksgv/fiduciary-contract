eecbbf77204037bb0fe68e40f656a70d75fb06e0
ericksgv <sangaravi22@gmail.com>
2023-04-11 20:12:56 -0500

    Contract attributes

--- contracts/Fiduciary.sol
+++ contracts/Fiduciary.sol
@@ -4,4 +4,43 @@ pragma solidity ^0.8.19;
 
 contract Fiduciary {
     
+    address regulator;
+	
+	struct Settlor {
+		address addressS;
+		uint amountMoney;
+	}
+	
+	struct Beneficiary {
+		address addressB; 
+		uint benefits;
+	}
+	
+	struct FiduciaryRelationship {
+		address settlor;
+		Beneficiary [] beneficiaries;
+		mapping (address => uint) beneficiariesP;
+		uint balance;
+		uint deadline;
+	}
+	
+	FiduciaryRelationship [] relations;
+	
+	constructor () {
+		regulator = msg.sender;
+	}
+	
+    function getRegulator () public view returns (address) {
+        return regulator;
+    }
+
+    function createFiduciaryRelationship (address _settlor) public {
+        require (regulator == msg.sender, "The regulator is the only who can create a fiduciary relationship");
+        FiduciaryRelationship storage relation = relations.push();
+        relation.settlor = _settlor;
+        
+    }
+
+	
+		
 }
